2a454ea6c200319acb764408c5e968a8820c1bb1
Aurélie <naoorelie@gmail.com>
2025-11-20 16:42:24 +0100

    barre latérale gauche

--- frontend/src/page/rootPage.tsx
+++ frontend/src/page/rootPage.tsx
@@ -1,12 +1,14 @@
 import {Outlet} from "react-router";
 import Header from "../component/header.tsx";
 import "../index.css";
+import LateralBar from "../component/lateralBar.tsx";
 
 export default function RootPage(){
 
     return (
         <>
             <Header/>
+            <LateralBar/>
             <Outlet/>
         </>
     )
